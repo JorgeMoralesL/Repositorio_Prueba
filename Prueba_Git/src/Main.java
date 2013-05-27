7324d7c57cccac10c445881af97499eb44b09569
jmorales <jmorales@E5500-FCTDESA2.axion.es>
2013-05-27 10:19:06 +0200

    Commit 2

--- Prueba_Git/src/Main.java
+++ Prueba_Git/src/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HOLA GIT!!!");
+		System.out.println("HOLA GIT 2222222!!!");
 	}
 
 }
